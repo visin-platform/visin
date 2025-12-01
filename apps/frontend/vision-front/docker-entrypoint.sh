c7379d9a4a47e1fc3233eda86fbcc2a2551f4a72
Toomas Tahves <toomastahves@hotmail.com>
2025-12-01 13:23:36 +0200

    fix: add missing variables to vision front

--- apps/frontend/vision-front/docker-entrypoint.sh
+++ apps/frontend/vision-front/docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 cat > /usr/share/nginx/html/config.json << EOF
 {
-  "VISION_API_URL": "${VISION_API_URL}"
+  "VISION_API_URL": "${VISION_API_URL}",
+  "AUTH_SERVICE_URL": "${AUTH_SERVICE_URL}",
+  "AUTH_FRONT_URL": "${AUTH_FRONT_URL}",
+  "GROUP_SERVICE_URL": "${GROUP_SERVICE_URL}"
 }
 EOF
 
